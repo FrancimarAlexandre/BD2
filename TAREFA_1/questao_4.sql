4d0466ad681ecd428bac12650cfd2eb3e443eeb4
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-13 18:35:56 -0300

    nova questão

--- TAREFA_1/questao_4.sql
+++ TAREFA_1/questao_4.sql
@@ -1,2 +1,4 @@
 -- Faça uma consulta que selecione o nome, o salário e o departamento dos funcionários que não são gerentes, ordenando pelo Código do Departamento.
 
+SELECT nome, salario, coddepto FROM funcionario
+WHERE codsupervisor IS null ORDER BY coddepto 
